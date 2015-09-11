c62ef15fd54dad8e6ebdca472dcf3a4120d05e86
Alexander Shaduro <belpyro@gmail.com>
2015-09-11 13:27:15 +0300

    + added new methods for actions and events

--- KspHelper/KspHelper/Actions/ActionsHelper.cs
+++ KspHelper/KspHelper/Actions/ActionsHelper.cs
@@ -4,6 +4,9 @@ using Fasterflect;
 
 namespace KspHelper.Actions
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public static class ActionsHelper
     {
         /// <summary>
@@ -12,9 +15,10 @@ namespace KspHelper.Actions
         /// <param name="module">current part module</param>
         /// <param name="name">action name (Custom01 for example)</param>
         /// <param name="action">link to a new action delegate</param>
-        public static void ReplaceAction(this PartModule module, string name, BaseActionDelegate action)
+        /// <param name="isGuiName"></param>
+        public static void ReplaceAction(this PartModule module, string name, BaseActionDelegate action, bool isGuiName = false)
         {
-            UpdateCallback(module, name, action);
+            UpdateCallback(module, name, action, isGuiName);
         }
 
         /// <summary>
@@ -23,38 +27,72 @@ namespace KspHelper.Actions
         /// <param name="module">current part module</param>
         /// <param name="name">action name (Custom01 for example)</param>
         /// <param name="action">link to a new action delegate</param>
-        public static void CombineAction(this PartModule module, string name, BaseActionDelegate action)
+        /// <param name="isGuiName"></param>
+        public static void CombineAction(this PartModule module, string name, BaseActionDelegate action, bool isGuiName = false)
         {
-            UpdateCallback(module, name, action, false);
+            UpdateCallback(module, name, action, false, isGuiName);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="module"></param>
+        /// <param name="name"></param>
+        /// <param name="isGuiName"></param>
+        /// <returns></returns>
+        public static ActionInfo GetAction(this PartModule module, string name, bool isGuiName = false)
+        {
+            if (!module.Actions.Any(a => a.name.Equals(name, StringComparison.InvariantCultureIgnoreCase))
+                && !module.Actions.Any(a => a.guiName.Equals(name, StringComparison.InvariantCultureIgnoreCase))) return null;
+
+            BaseAction oldAction = isGuiName ? module.Actions.SingleOrDefault(a => a.guiName.Equals(name, StringComparison.InvariantCultureIgnoreCase)) : module.Actions[name];
+
+            var callback = (BaseActionDelegate) oldAction?.TryGetPropertyValue("onEvent", Flags.InstanceAnyDeclaredOnly);
+
+            if (callback == null) return null;
+
+            return new ActionInfo() {Callback = callback, CallbackAction = oldAction};
         }
 
         private static void UpdateCallback(PartModule module, string name, BaseActionDelegate action,
-            bool replace = true)
+            bool replace = true, bool isGuiName = false)
         {
-            if (!module.Actions.Any(a => a.name.Equals(name, StringComparison.InvariantCultureIgnoreCase))) return;
+            ActionInfo info = module.GetAction(name, isGuiName);
 
-            BaseAction oldAction = module.Actions[name];
-
-            var callback = (BaseActionDelegate) oldAction.TryGetPropertyValue("onEvent", Flags.InstanceAnyDeclaredOnly);
-
-            if (callback == null) return;
+            if (info == null) return;
 
             BaseAction newAction;
 
             if (replace)
             {
                 newAction = new BaseAction(module.Actions, name, action,
-                    new KSPAction(oldAction.guiName, oldAction.actionGroup));
+                    new KSPAction(info.CallbackAction.guiName, info.CallbackAction.actionGroup));
             }
             else
             {
-                var combined = (BaseActionDelegate) Delegate.Combine(callback, action);
+                var combined = (BaseActionDelegate) Delegate.Combine(info.Callback, action);
                 newAction = new BaseAction(module.Actions, name, combined,
-                    new KSPAction(oldAction.guiName, oldAction.actionGroup));
+                    new KSPAction(info.CallbackAction.guiName, info.CallbackAction.actionGroup));
             }
 
-            module.Actions.Remove(oldAction);
+            module.Actions.Remove(info.CallbackAction);
             module.Actions.Add(newAction);
         }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ActionInfo
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public BaseActionDelegate Callback { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public BaseAction CallbackAction { get; set; }
     }
 }
--- KspHelper/KspHelper/Events/EventsHelper.cs
+++ KspHelper/KspHelper/Events/EventsHelper.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Linq;
 using Fasterflect;
 
 namespace KspHelper.Events
 {
+    /// <summary>
+    /// Helper for managing KspEvents
+    /// </summary>
     public static class EventsHelper
     {
         /// <summary>
@@ -11,9 +15,10 @@ namespace KspHelper.Events
         /// <param name="module">current module</param>
         /// <param name="name">event name ("Activate" for example)</param>
         /// <param name="replaceAction">new action delegate</param>
-        public static void ReplaceEvent(this PartModule module, string name, BaseEventDelegate replaceAction)
+        /// <param name="isGuiName">name is guiName of event</param>
+        public static void ReplaceEvent(this PartModule module, string name, BaseEventDelegate replaceAction, bool isGuiName = false)
         {
-            UpdateEvent(module, name, replaceAction);
+            UpdateEvent(module, name, replaceAction, isGuiName);
         }
 
         /// <summary>
@@ -22,20 +27,17 @@ namespace KspHelper.Events
         /// <param name="module">current module</param>
         /// <param name="name">event name ("Activate" for example)</param>
         /// <param name="replaceAction">new action delegate</param>
-        public static void CombineEvent(this PartModule module, string name, BaseEventDelegate replaceAction)
+        /// <param name="isGuiName"></param>
+        public static void CombineEvent(this PartModule module, string name, BaseEventDelegate replaceAction, bool isGuiName = false)
         {
-            UpdateEvent(module, name, replaceAction, false);
+            UpdateEvent(module, name, replaceAction, false, isGuiName);
         }
 
-        private static void UpdateEvent(PartModule module, string name, BaseEventDelegate action, bool replace = true)
+        private static void UpdateEvent(PartModule module, string name, BaseEventDelegate action, bool replace = true, bool isGuiName = false)
         {
-            if (!module.Events.Contains(name)) return;
+            EventInfo info = GetEvent(module, name, isGuiName);
 
-            var oldEvent = module.Events[name];
-
-            var callback = oldEvent.TryGetPropertyValue("onEvent");
-
-            if (callback == null) return;
+            if (info == null) return;
 
             BaseEvent baseEvent;
 
@@ -45,34 +47,46 @@ namespace KspHelper.Events
             }
             else
             {
-                var combinedDelegate = (BaseEventDelegate)Delegate.Combine((Delegate)callback, action);
+                var combinedDelegate = (BaseEventDelegate)Delegate.Combine((Delegate)info.CallBack, action);
                 baseEvent = new BaseEvent(module.Events, name, combinedDelegate);
             }
 
-            oldEvent.CopyDataTo(baseEvent);
+            info.CallbackEvent.MapFields(baseEvent);
 
-            module.Events.Remove(oldEvent);
-            module.Events.Add(baseEvent); 
+            module.Events.Remove(info.CallbackEvent);
+            module.Events.Add(baseEvent);
         }
 
         /// <summary>
-        /// Full copy of KSPEvent data to a new event
+        /// 
         /// </summary>
-        /// <param name="source">event source</param>
-        /// <param name="dest">event destination</param>
-        public static void CopyDataTo(this BaseEvent source, BaseEvent dest)
+        /// <param name="module"></param>
+        /// <param name="name"></param>
+        /// <param name="isGuiName"></param>
+        /// <returns></returns>
+        public static EventInfo GetEvent(this PartModule module, string name, bool isGuiName = false)
         {
-            dest.active = source.active;
-            dest.category = source.category;
-            dest.externalToEVAOnly = source.externalToEVAOnly;
-            dest.guiActive = source.guiActive;
-            dest.guiActiveEditor = source.guiActiveEditor;
-            dest.guiActiveUnfocused = source.guiActiveUnfocused;
-            dest.guiIcon = source.guiIcon;
-            dest.guiName = source.guiName;
-            dest.unfocusedRange = source.unfocusedRange;
+            if (!module.Events.Contains(name) && !module.Events.Any(e => e.GUIName.Equals(name, StringComparison.InvariantCultureIgnoreCase))) return null;
+
+            var oldEvent = isGuiName ? module.Events.SingleOrDefault(e => e.GUIName.Equals(name, StringComparison.InvariantCultureIgnoreCase)) : module.Events[name];
+
+            var callback = oldEvent?.TryGetPropertyValue("onEvent") as BaseEventDelegate;
+
+            if (callback == null) return null;
+
+            return new EventInfo()
+            {
+                CallBack = callback,
+                CallbackEvent = oldEvent
+            };
         }
 
 
+    }
+    public class EventInfo
+    {
+        public BaseEventDelegate CallBack { get; set; }
+
+        public BaseEvent CallbackEvent { get; set; }
     }
 }
