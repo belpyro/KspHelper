d4632f1cdeabcd84dcea0a65fdb5bc13d4d04f43
Aleksander Shaduro <aleksander_shaduro@epam.com>
2016-10-09 21:25:49 +0300

    + added ILMerge to build options - remove ol code + added GuiBehavior

---
+++ KspHelper/KspHelper/Behavior/GuiBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace KspHelper.Behavior
+{
+    public class GuiBehavior: KspBehavior
+    {
+        public Action Draw { get; set; }
+
+        protected override void OnGUI()
+        {
+            Draw?.Invoke();
+        }
+    }
+
+    public class GuiBehavior<T>: KspBehavior where T: new() 
+    {
+        private T _data;
+        private bool _isInitialized;
+
+        public void Initialize(T data)
+        {
+            _data = data;
+            _isInitialized = true;
+        }
+
+        public Action<T> Draw { get; set; }
+
+        protected override void OnGUI()
+        {
+            if (!_isInitialized)
+            {
+                throw new InvalidOperationException("Parameter data is not initialized");
+            }
+
+            Draw?.Invoke(_data);
+        }
+    }
+
+
+}
---
+++ KspHelper/KspHelper/Windows/ModWindow.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using KspHelper.Interfaces;
+using UnityEngine;
+using Random = System.Random;
+
+namespace KspHelper.Windows
+{
+    public abstract class ModLayoutWindow : ScenarioModule, IGuiBehavior
+    {
+        private readonly int _id = new Random().Next(123, 99999);
+        private Rect _rect = GUIUtil.ScreenCenteredRect(100f, 100f);
+        private bool _isVisible;
+        private string _title = "";
+        private bool _isClamp;
+
+        public override void OnAwake()
+        {
+            base.OnAwake();
+            WindowSettingsAttribute attr = Attribute.GetCustomAttribute(this.GetType(), typeof(WindowSettingsAttribute)) as WindowSettingsAttribute;
+            if (attr != null)
+            {
+                _rect = GUIUtil.ScreenCenteredRect(attr.Width, attr.Height);
+                _title = attr.Title;
+                _isClamp = attr.ClampToRect;
+            }
+        }
+
+        public void OnGUI()
+        {
+            if (!_isVisible) return;
+            _rect = GUILayout.Window(_id, _isClamp ? KSPUtil.ClampRectToScreen(_rect) : _rect, DrawWindow, _title, HighLogic.Skin.window);
+            GUI.BringWindowToFront(_id);
+        }
+
+        private void DrawWindow(int id)
+        {
+            InternalDraw();
+            GUI.DragWindow();
+        }
+
+        protected abstract void InternalDraw();
+
+        public void Show()
+        {
+            _isVisible = true;
+        }
+
+        public void Hide()
+        {
+            _isVisible = true;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class WindowSettingsAttribute : Attribute
+    {
+        public WindowSettingsAttribute()
+        {
+            Width = 100;
+            Height = 100;
+            ClampToRect = true;
+            Title = "";
+        }
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public bool ClampToRect { get; set; }
+
+        public string Title { get; set; }
+    }
+}
